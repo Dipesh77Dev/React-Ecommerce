397ad731f57613ff3c89741351a21bfaef60eeb9
Dipesh77Dev <rajpersonal777@gmail.com>
2022-08-25 11:02:07 +0530

    started

---
+++ ecommercewebsite/src/App.js
@@ -0,0 +1,13 @@
+import NavigationBar from './Components/NavigationBar';
+
+function App() {
+  return (
+    <>
+      {/* <h1>App</h1> */}
+      <NavigationBar />
+    </>
+  );
+
+}
+
+export default App;
